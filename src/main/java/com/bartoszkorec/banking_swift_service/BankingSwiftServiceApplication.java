bb9e6a16e87991bd19b191fd4985b9d37bf877a5
BartoszKorec <bartosz.korec@student.pk.edu.pl>
2025-01-30 18:54:20 +0100

    Initial commit

---
+++ src/main/java/com/bartoszkorec/banking_swift_service/BankingSwiftServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bartoszkorec.banking_swift_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSwiftServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSwiftServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartoszkorec/banking_swift_service/BankingSwiftServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bartoszkorec.banking_swift_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class BankingSwiftServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
